dbb9df8fcfa896c0d996bd2516df7d158c4ca893
KashifKhn <kashifkhnx04@gmail.com>
2025-01-15 19:28:08 +0500

    all command is invalid

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,12 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from stdin")
+		return
+	}
+
+	fmt.Printf("%s: command not found\n", strings.Split(reader, "\n")[0])
+
 }
