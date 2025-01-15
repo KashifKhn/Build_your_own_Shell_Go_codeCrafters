ae07073275a3031ee6361e1ea3885829b93f1b49
KashifKhn <kashifkhnx04@gmail.com>
2025-01-15 20:18:05 +0500

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,14 +11,29 @@ func main() {
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from stdin")
-			return
+		reader := userInput()
+		commad := extractCommand(reader)
+
+		if strings.HasPrefix(commad, "exit") {
+			os.Exit(0)
 		}
-
-		fmt.Printf("%s: command not found\n", strings.Split(reader, "\n")[0])
-
+		commandNotFound(commad)
 	}
+}
 
+func userInput() string {
+	reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from stdin")
+		os.Exit(1)
+	}
+	return reader
+}
+
+func commandNotFound(command string) {
+	fmt.Printf("%s: command not found\n", command)
+}
+
+func extractCommand(reader string) string {
+	return strings.Split(strings.ToLower(strings.TrimSpace(reader)), "\n")[0]
 }
